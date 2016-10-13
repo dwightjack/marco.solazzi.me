2dc7b130d9e6679e9d59a8fb3e082a67b269662c
Marco Solazzi <marco.solazzi@gmail.com>
2016-10-13 23:18:16 +0200

    cover img

--- application/assets/js/components/Cover/index.js
+++ application/assets/js/components/Cover/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './_cover.scss';
 
 import Table, { IcoTable } from '../Table';
+import pic from '../../../images/marco.jpg';
 
 const data = {
     name: 'Marco',
@@ -32,7 +33,20 @@ const Cover = () => (
 */}
     <section className="c-section c-cover__body">
 
+        <figure className="c-cover__pic">
+            <svg viewBox="0 0 890 890">
+                <path className="stroke" d="M850 425L725.5 725.5 425 850 124.5 725.5 0 425l124.5-300.5L425 0l300.5 124.5z" />
+                <g clipPath="url(#profile-clip-path)">
+                    <image xlinkHref={pic} x="0" y="0" width="850" height="860" />
+                </g>
+                <clipPath id="profile-clip-path">
+                    <path d="M850 425L725.5 725.5 425 850 124.5 725.5 0 425l124.5-300.5L425 0l300.5 124.5z" />
+                </clipPath>
+            </svg>
+        </figure>
+
         <div className="c-section__body">
+
             <h2 className="c-cover__title">こんにちわ！</h2>
             <Table
                 data={data}
