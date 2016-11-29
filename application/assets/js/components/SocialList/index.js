8d33986fe50c07e717aa4ab8e7749b2cb7430ce7
Marco Solazzi <marco.solazzi+git@aquest.it>
2016-11-29 18:00:42 +0100

    print only stuff

--- application/assets/js/components/SocialList/index.js
+++ application/assets/js/components/SocialList/index.js
@@ -4,10 +4,15 @@ import List, { ListItem } from '../../components/List';
 import Ico from '../../components/Ico';
 import social from '../../database/social.json';
 
+const classes = {
+    link: 'u-only-print',
+    donwload: 'u-only-screen'
+};
+
 const SocialList = () => (
     <List className="o-social-list">
         {social.map(({type, url, label}) => (
-            <ListItem key={type}>
+            <ListItem key={type} className={classes[type]}>
                 <Ico name={type} /> <a href={url} target="_blank" rel="noopener noreferrer">{label}</a>
             </ListItem>
         ))}
