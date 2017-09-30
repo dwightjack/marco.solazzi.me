253c1d668d857ae667a37845141f65466461d752
Marco Solazzi <marco.solazzi@gmail.com>
2017-09-30 22:10:55 +0200

    parallax tests

--- app/assets/js/components/Section/index.vue
+++ app/assets/js/components/Section/index.vue
@@ -1,7 +1,8 @@
 <template>
     <section :class="$style.root">
         <SectionTitle :class="$style.title" :prefix="prefix" :title="title" :subtitle="subtitle" />
-        <div :class="$style.body" :style="{ transform: `translateY(${scrollAmount}px)` }">
+        <div :class="$style.body" ref="body" :style="{ transform: `translate3d(0, ${scrollAmount}px, 0)` }">
+            <!--   -->
             <slot />
         </div>
     </section>
@@ -9,14 +10,15 @@
 
 <script>
 import VueTypes from 'vue-types';
+//import anime from 'animejs';
+import { mapState } from 'vuex';
 import SectionTitle from '@/objects/Title';
 
 export default {
 
     data() {
         return {
-            rootOffsetTop: 0,
-            pagelistScroll: 0
+            rootOffsetTop: 0
         };
     },
 
@@ -30,7 +32,25 @@ export default {
         SectionTitle
     },
 
+    // TODO: should check performances on the final product
+    // watch: {
+    //     scrollAmount(translateY) {
+
+    //         requestAnimationFrame(() => {
+    //             anime({
+    //                 targets: this.$refs.body,
+    //                 duration: 0,
+    //                 translateY,
+    //                 translateZ: 0
+    //             });
+    //         });
+
+    //     }
+    // },
+
     computed: {
+        ...mapState(['pagelistScroll']),
+
         scrollAmount() {
             const amount = (this.rootOffsetTop + parseInt(window.innerHeight * 0.2, 10)) - (this.pagelistScroll + (window.innerHeight / 2));
             return Math.floor(amount * 0.1);
@@ -38,7 +58,10 @@ export default {
     },
 
     mounted() {
-        this.rootOffsetTop = this.$el.offsetTop;
+        setTimeout(() => {
+            this.rootOffsetTop = this.$el.offsetTop;
+        }, 100);
+
     }
 };
 </script>
