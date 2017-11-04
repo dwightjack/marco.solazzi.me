a340eb51937ffa8d4f767372789a9d04c981db2f
Marco Solazzi <marco.solazzi@gmail.com>
2017-11-04 22:53:20 +0100

    build(webpack): correct folder for custom config

--- config/webpack/webpack.base.js
+++ config/webpack/webpack.base.js
@@ -7,7 +7,7 @@ let baseConfig;
 
 try {
     baseConfig = require('umeboshi-config-vue/webpack/webpack.base');
-} catch () {
+} catch (e) {
     baseConfig = require('umeboshi-config/webpack/webpack.base');
 }
 
@@ -17,5 +17,11 @@ baseConfig.plugins.push(
         to: paths.toAbsPath('dist.root')
     }])
 );
+
+const vueLoader = baseConfig.module.rules.find(({ loader }) => loader === 'vue-loader');
+
+if (vueLoader) {
+    vueLoader.options.cacheBusting = false;
+}
 
 module.exports = baseConfig;
