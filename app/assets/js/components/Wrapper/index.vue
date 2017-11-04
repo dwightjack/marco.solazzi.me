e51c4aeeb7c0acc794a7fa1b04aed63c0f947548
Marco Solazzi <marco.solazzi@gmail.com>
2017-11-04 19:29:39 +0100

    refactor(wrapper): functional component
    removed unused methods

--- app/assets/js/components/Wrapper/index.vue
+++ app/assets/js/components/Wrapper/index.vue
@@ -1,61 +1,7 @@
-<template>
-    <main role="main" :class="$style.root" @scroll="onScroll">
+<template functional>
+    <main role="main" :class="$style.root">
         <slot />
-        <slot name="pagelist" />
     </main>
 </template>
-
-<script>
-import VueTypes from 'vue-types';
-import { mapState } from 'vuex';
-
-export default {
-
-    props: {
-        onPageChange: VueTypes.func
-    },
-
-    computed: {
-
-        ...mapState({
-            currentPage: (state) => state.ui.route
-        }),
-
-        pages() {
-            const windowHeightOffset = parseInt(window.innerHeight * 0.25, 10);
-            const { pagelist } = this.$slots;
-
-            return pagelist[0].$children.map((vm) => ({
-                offsetTop: vm.$el.offsetTop - windowHeightOffset,
-                route: vm.$el.pageName,
-                el: vm.$el
-            })).reverse();
-        }
-    },
-
-    watch: {
-        currentPage(newVal, oldVal) {
-            if (newVal !== oldVal) {
-                this.onPageChange(newVal, true);
-            }
-        }
-    },
-
-    methods: {
-        onScroll(e) {
-            if (this.$mq.matches('tablet')) {
-                const { scrollTop } = e.target;
-                this.pages.some(({ offsetTop, route }) => {
-                    if (offsetTop < scrollTop) {
-                        this.currentPage = route;
-                        return true;
-                    }
-                    return false;
-                });
-            }
-        }
-    }
-};
-</script>
 
 <style lang="scss" module src="./wrapper.scss" />
