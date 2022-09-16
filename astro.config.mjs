e69e3d57c41958368514bebcb66b4d5e214bca39
Marco Solazzi <marco.solazzi@gmail.com>
2022-09-16 10:52:07 +0900

    chore: support files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://marco.solazzi.me",
+  site: 'https://marco.solazzi.me',
   integrations: [vue()],
 });
