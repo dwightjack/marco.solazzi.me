6c65b84e0f6a646b865fe288eede932a881d5e07
Marco Solazzi <marco.solazzi@gmail.com>
2022-09-21 16:04:58 +0900

    cover and data import

---
+++ src/shared/constants.ts
@@ -0,0 +1,11 @@
+// NAV PATHS
+
+export const NAV_PATH_HOME = 'home';
+export const NAV_PATH_JOBS = 'jobs';
+export const NAV_PATH_EDUCATION = 'education';
+export const NAV_PATH_SKILLS = 'skills';
+export const NAV_PATH_PORTFOLIO = 'portfolio';
+
+export const GROUP_LOADER = 'loader';
+export const GROUP_COVER = 'cover';
+export const GROUP_PAGELIST = 'pagelist';
