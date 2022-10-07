4022d9a28a9b0af8e1712a264a14d8c520a20302
Marco Solazzi <marco.solazzi@gmail.com>
2022-10-07 14:09:19 +0900

    chore: remove vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import vue from '@astrojs/vue';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marco.solazzi.me',
-  integrations: [vue()],
   vite: {
     ssr: {
       external: ['svgo'],
