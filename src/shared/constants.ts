2f43cfbafba1789369a589286cea85fc53e78771
Marco Solazzi <marco.solazzi@gmail.com>
2022-11-01 19:20:16 +0900

    feat: theme switcher and primitives review

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -5,3 +5,6 @@ export const NAV_PATH_EDUCATION = 'education';
 export const NAV_PATH_SKILLS = 'skills';
 export const NAV_PATH_WORKS = 'works';
 export const NAV_PATH_TALKS = 'talks';
+
+export const THEMES = ['red', 'green', 'purple', 'blue'] as const;
+export const THEME_DEFAULT = 'red';
