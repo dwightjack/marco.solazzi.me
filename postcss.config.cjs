6eaa07e3d6b9bd4ef93dda9133df0f025d34421a
dwightjack <marco.solazzi@gmail.com>
2023-03-16 14:09:22 +0900

    fix: postcss breaking change

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,15 +3,16 @@
 
 const presetEnv = require('postcss-preset-env');
 const { resolve } = require('path');
+const postcssGlobalData = require('@csstools/postcss-global-data');
 module.exports = {
   plugins: [
+    postcssGlobalData({ files: [resolve(__dirname, 'src/styles/tokens.css')] }),
     presetEnv({
       stage: 2,
       features: {
         'custom-properties': false,
         'nesting-rules': true,
       },
-      importFrom: resolve(__dirname, 'src/styles/tokens.css'),
     }),
   ],
 };
