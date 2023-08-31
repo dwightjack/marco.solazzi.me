8fd45867686bac099f01d814a12d5fcd0b188109
dwightjack <marco.solazzi@gmail.com>
2023-08-31 14:26:00 +0900

    fix integration setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:4321',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -55,6 +55,6 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: process.env.CI ? 'pnpm run preview' : 'pnpm run dev',
-    port: 3000,
+    port: 4321,
   },
 });
