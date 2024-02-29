8c7249839330cdba53c7f36cabe3326704a2db29
dwightjack <marco.solazzi@gmail.com>
2024-02-29 14:57:05 +0900

    chore: stack update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marco.solazzi.me',
   compressHTML: false,
-  vite: {
-    ssr: {
-      external: ['svgo'],
-    },
-  },
+  integrations: [icon()],
 });
