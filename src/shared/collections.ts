31c90ee3a22cfa71a1c51000bd752cdc69f98518
dwightjack <marco.solazzi@gmail.com>
2024-10-21 11:55:04 +0900

    show draft articles in preview mode

--- src/shared/collections.ts
+++ src/shared/collections.ts
@@ -120,7 +120,7 @@ export async function getTalks() {
 export async function getBlogPosts() {
   return (await getCollection('blog'))
     .filter((entry) => {
-      if (import.meta.env.PREVIEW === 'true') {
+      if (import.meta.env.PUBLIC_PREVIEW === 'true') {
         return true;
       }
       return import.meta.env.PROD ? entry.data.isDraft !== true : true;
