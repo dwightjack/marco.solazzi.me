076398eaa36e8423593bf4abfae9abe050d5c495
dwightjack <marco.solazzi@gmail.com>
2025-03-26 11:14:06 +0900

    add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import icon from 'astro-icon';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkDirective from 'remark-directive';
 import { remarkContainersPlugin } from './build/remark-plugins.mjs';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marco.solazzi.me',
   compressHTML: false,
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
