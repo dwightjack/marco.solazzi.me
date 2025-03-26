57e4c2c99fdb33ebf92a2b71e835d0db4d7f7e2e
dwightjack <marco.solazzi@gmail.com>
2025-03-26 10:29:40 +0900

    refactor and cross browser support

--- src/shared/patterns/tableScroller.ts
+++ src/shared/patterns/tableScroller.ts
@@ -1,24 +1,54 @@
 export function initialize() {
   const scrollers = document.querySelectorAll('.p-table-scroller');
+  const supportsScrollTimeline = CSS.supports('animation-timeline', '--test');
 
   if (scrollers.length === 0) {
     return;
   }
 
-  const resizeObserver = new ResizeObserver((entries) => {
-    for (const entry of entries) {
-      if (entry.contentBoxSize) {
-        const canScroll =
-          getComputedStyle(entry.target, '::before').getPropertyValue(
-            'visibility',
-          ) === 'visible';
+  function fallbackScroll(container: HTMLElement) {
+    const table = container.querySelector('table');
+    if (!table) {
+      return;
+    }
 
-        if (canScroll) {
-          entry.target.setAttribute('tabindex', '0');
-          return;
-        }
-        entry.target.removeAttribute('tabindex');
+    function onScroll() {
+      container.style.setProperty(
+        '--reveal-start',
+        container.scrollLeft > 0 ? '1' : '0',
+      );
+      container.style.setProperty(
+        '--reveal-end',
+        container.scrollLeft < scrollMax ? '1' : '0',
+      );
+    }
+
+    const scrollMax = table.offsetWidth - container.offsetWidth;
+    const shouldScroll = table.offsetWidth > container.offsetWidth;
+    container.style.setProperty('--can-scroll', shouldScroll ? ' ' : 'initial');
+    onScroll();
+    container.removeEventListener('scroll', onScroll);
+    if (shouldScroll) {
+      container.addEventListener('scroll', onScroll);
+    }
+  }
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    if (supportsScrollTimeline === false) {
+      entries.forEach(({ target }) => fallbackScroll(target as HTMLElement));
+    }
+    for (const entry of entries) {
+      const canScroll =
+        getComputedStyle(entry.target, '::before').getPropertyValue(
+          'visibility',
+        ) === 'visible';
+      console.log(canScroll);
+
+      if (canScroll) {
+        entry.target.setAttribute('tabindex', '0');
+        return;
       }
+      entry.target.removeAttribute('tabindex');
     }
   });
 
