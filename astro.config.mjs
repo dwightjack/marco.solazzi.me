446872d71a387c6e863843f409ef59c0319671c0
dwightjack <marco.solazzi@gmail.com>
2025-04-07 14:36:01 +0900

    use high contrast theme for code snippets

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,8 +13,8 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       themes: {
-        light: 'github-light',
-        dark: 'github-dark',
+        light: 'github-light-high-contrast',
+        dark: 'github-dark-high-contrast',
       },
     },
     remarkPlugins: [remarkDirective, remarkContainersPlugin],
