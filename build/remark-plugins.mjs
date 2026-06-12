79093da2463cbc16150d4002127560f29b096e4f
dwightjack <marco.solazzi@gmail.com>
2026-06-12 15:53:25 +0900

    codepen embed

--- build/remark-plugins.mjs
+++ build/remark-plugins.mjs
@@ -27,6 +27,27 @@ let idx = 0;
 /** @type {Plugin<{}>} */
 export function remarkContainersPlugin() {
   return function (tree) {
+    visit(tree, { type: 'leafDirective' }, function (node, index, parent) {
+      if (node.name !== 'codepen') return;
+      const {
+        user,
+        id,
+        height = '300',
+        tab = 'html,result',
+      } = node.attributes || {};
+      const title = node.children?.[0]?.value ?? id;
+      const penUrl = `https://codepen.io/${user}/pen/${id}`;
+      const userUrl = `https://codepen.io/${user}`;
+      const pHtml = `<p class="codepen" data-height="${height}" data-pen-title="${title}" data-default-tab="${tab}" data-slug-hash="${id}" data-user="${user}" style="height: ${height}px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"><span>See the Pen <a href="${penUrl}">${title}</a> by <a href="${userUrl}">@${user}</a> on <a href="https://codepen.io">CodePen</a>.</span></p>`;
+      const scriptHtml = `<script async src="https://public.codepenassets.com/embed/index.js"></script>`;
+      parent.children.splice(
+        index,
+        1,
+        { type: 'html', value: pHtml },
+        { type: 'html', value: scriptHtml },
+      );
+    });
+
     visit(tree, { type: 'containerDirective' }, function (node) {
       if (node.name === 'pullquote') {
         return transformNode(node, 'figure', { class: 'pullquote' });
